e6997301af49dece1a9b4a53c10be4b1ec57c635
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-06-11 15:53:05 -0500

    completed format of initial animals page

--- src/routes/Animals.js
+++ src/routes/Animals.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import Card from '../components/UI/Card'
+import BSF from '../images/BSF.jpg'
+import chicken from '../images/chicken.jpg'
+import reptiles from '../images/reptiles.jpg'
+import Layout from '../components/Layout/Layout'
+import { Divider } from 'antd'
+import '../components/Animals/Animals.css'
 
 const Animals = () => {
-  return <div>Animals</div>
+  return (
+    <Layout>
+      <div className="card-container">
+        <Card>
+          <img className="main-img" src={chicken} />
+          <Divider />
+          <h2>Fowl</h2>
+        </Card>
+        <Card>
+          <img className="main-img" src={reptiles} />
+          <Divider />
+          <h2>Reptiles</h2>
+        </Card>
+        <Card>
+          <img className="main-img" src={BSF} />
+          <Divider />
+          <h2>Bugs</h2>
+        </Card>
+      </div>
+    </Layout>
+  )
 }
 
 export default Animals
+
