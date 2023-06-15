b011912565ac535f424a5a08e1ae86262ad0c4f4
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-06-15 17:41:05 -0500

    added contact card

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,5 +1,10 @@
 import { Form } from 'antd'
 import React from 'react'
+import Card from '../UI/Card'
+import twitter from '../../images/twitter.png'
+import facebook from '../../images/facebook.png'
+import instagram from '../../images/instagram.png'
+import '../Contact/Contact.css'
 
 const ContactForm = () => {
   return <div>
@@ -7,8 +12,11 @@ const ContactForm = () => {
     <h2>We Want to Hear From You!</h2>
     <p>Leave us any feedback, questions, comments or concerns below and we will contact you soon.</p>
     <label>Don't forget to like and subscribe:</label>
-    <a target="_blank"><img /></a>
-    <a target="_blank"><img /></a>
+    <div className="contact-icon-container">
+    <a target="_blank"><img className="contact-icon"src={twitter}/></a>
+    <a target="_blank"><img className="contact-icon"src={facebook}/></a>
+    <a target="_blank"><img className="contact-icon"src={instagram}/></a>
+    </div>
     </Card>
     <Form>
 
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import ContactForm from '../components/Contact/ContactForm'
 
 const Contact = () => {
   return (
     <Layout>
-      <div>Contact</div>
+     <ContactForm />
     </Layout>
   )
 }
