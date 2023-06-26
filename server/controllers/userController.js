da793654043f47698e1f2b97df83d1382ee857f7
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-06-26 14:56:20 -0500

    created jwt token for userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,36 @@
-const test = (req, res) => {
-    res.sendStatus(200)
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const SECRET = process.env.SECRET
+
+const generateToken = credentials => {
+  return jwt.sign(
+    {
+      username: credentials.username,
+      password: credentials.password
+    },
+    SECRET,
+    {
+      expiresIn: '24 hours'
+    }
+  )
 }
 
-module.exports = {test}
+const createToken = async (req, res) => {
+  console.log('createToken', req.body)
+  let token = generateToken(req.body)
+  res.status(200).send(token)
+}
+
+const validateToken = async (req, res) => {
+  let token = req.get('Authorization')
+  let valid = jwt.verify(token, SECRET)
+  if (valid) {
+    res.status(200).send('Success.Token Valid')
+  } else {
+    res.status(400).send('Token invalid.')
+  }
+  console.log('Validate Token Body', req.body)
+}
+
+module.exports = { createToken, validateToken }
