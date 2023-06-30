2a693d89f65ee4a174748ac9ec8f11f59924fdce
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-06-30 13:58:13 -0500

    created shipping section

--- src/components/Checkout/Shipping.js
+++ src/components/Checkout/Shipping.js
@@ -1,9 +1,101 @@
 import React from 'react'
+import { Divider } from 'antd'
+import Card from '../UI/Card'
+import './Shipping.css'
 
-const Shipping = () => {
-    return(
-        <div>Shipping</div>
-    )
+const Shipping = ({
+  email,
+  setEmail,
+  firstName,
+  setFirstName,
+  lastName,
+  setLastName,
+  address,
+  setAddress,
+  city,
+  setCity,
+  zipcode,
+  setZipcode,
+  phone,
+  setPhone,
+  state,
+  setState
+}) => {
+  return (
+    <Card>
+      <form className="shipping-form-container">
+        <h1>Shipping Details:</h1>
+        <Divider />
+        <div className="name">
+          <input
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+            className="shipping-input"
+            placeholder="First Name:"
+            required
+          />
+          <input
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+            className="shipping-input"
+            placeholder="Last Name:"
+            required
+          />
+        </div>
+
+        <input
+          value={address}
+          onChange={e => setAddress(e.target.value)}
+          className="shipping-input address"
+          placeholder="Address:"
+          required
+        />
+        <div className="city-state-zip">
+          <input
+            value={city}
+            onChange={e => setCity(e.target.value)}
+            className="shipping-input city"
+            placeholder="City:"
+            required
+          />
+
+          <input
+            value={state}
+            onChange={e => setState(e.target.value)}
+            className="shipping-input state"
+            placeholder="State:"
+            required
+          />
+
+          <input
+            value={zipcode}
+            onChange={e => setZipcode(e.target.value)}
+            className="shipping-input zip"
+            placeholder="Zip Code:"
+            required
+          />
+        </div>
+
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="shipping-input address"
+          type="email"
+          placeholder="Email:"
+          required
+        />
+
+        <input
+          value={phone}
+          onChange={e => setPhone(e.target.value)}
+          className="shipping-input phone"
+          placeholder="Phone:"
+          required
+        />
+        <button className="save-info">Save</button>
+      </form>
+    </Card>
+  )
 }
 
 export default Shipping
