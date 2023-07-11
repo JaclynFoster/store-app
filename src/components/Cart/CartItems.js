e5b99a3bdb9af4ea82e76308323622e9b2002ae3
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-07-10 19:03:24 -0500

    cart styling finalized. Add to cart functionality complete. Pricing functions added. All mapped information correct.

--- src/components/Cart/CartItems.js
+++ src/components/Cart/CartItems.js
@@ -4,13 +4,12 @@ import { IoBagCheckOutline, IoLogoHtml5 } from 'react-icons/io5'
 import { ImCancelCircle } from 'react-icons/im'
 import { MdOutlineShopTwo } from 'react-icons/md'
 import { Row, Col, Button, Container } from 'react-bootstrap'
-import { Divider,Card } from 'antd'
+import { Divider, Card } from 'antd'
 import '../Cart/CartItems.css'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { cartOptions, changeQuantity } from '../../redux/slices/cartItemSlice'
 import { MinusOutlined, PlusOutlined } from '@ant-design/icons'
-
 
 const CartItems = ({ breed }) => {
   const dispatch = useDispatch()
@@ -18,25 +17,28 @@ const CartItems = ({ breed }) => {
   const count = useSelector(state => state.cart.value)
   const navigate = useNavigate()
 
+  const calculateQuantity = (quantity, price) => {
+    return quantity * Number(price)
+  }
+
   const totalCartAmount = () => {
-    return Object.values(cartAnimal).reduce((acc, curr) => {
-      return (acc + curr.price || 0)
-    },"")
+    console.log('Animal:', Object.values(cartAnimal))
+
+    return Object.values(cartAnimal)
+      .reduce((acc, curr) => {
+        return acc + calculateQuantity(curr.quantity, curr.price)
+      }, 0)
+      .toFixed(2)
   }
 
   const cartTax = () => {
-    return Object.values(cartAnimal).reduce((acc, curr) => {
-     console.log("Total Tax", acc + curr.price)
-     return (acc + curr.price * .08 || 0)
-    }, 0)
+    return (totalCartAmount() * 0.0825).toFixed(2)
   }
 
   const grandTotal = () => {
-    return Object.values(cartAnimal).reduce((acc, curr) => {
-      let total = (acc + curr.price) * 1.08 
-      console.log(total)
-      return (Math.round(total) || 0)
-    }, 0)
+    console.log('cartTax', typeof cartTax())
+    console.log('totalCartAmount', typeof totalCartAmount())
+    return Number(cartTax()) + Number(totalCartAmount())
   }
   const toCheckout = () => {
     navigate('/checkout', { replace: true })
@@ -61,119 +63,104 @@ const CartItems = ({ breed }) => {
             console.log('item', item)
           }
           return (
-            <Card>
-         
-         <Button className="trash" variant="outline-danger">
-                <BsTrash />
-              </Button>
-              <img className="table-img" src={item.breed_image} />
-              <div className="cart-detail-container">
-               
-                
-               
-              <h4>{item.breed_name}</h4>
-              
-            <div className="cart-price">
+            <div className="cart-detail-container">
+              <Card>
+                <Button className="trash" variant="outline-danger">
+                  <BsTrash />
+                </Button>
+                <img className="table-img" src={item.breed_image} />
+                <Row>
+                  <Col>
+                    <h4 className="cart-name">{item.breed_name}</h4>
+                  </Col>
 
-                <h5>Price:</h5>
-              <h5>${item.price}.00</h5>
+                  <Col>
+                    <h5>Price:</h5>
+                    <h5>${item.price}.00</h5>
+                  </Col>
+
+                  <Col>
+                    <h5>Qty:</h5>
+
+                    <div className="cart-qty">
+                      <MinusOutlined
+                        onClick={() =>
+                          dispatch(
+                            changeQuantity({
+                              id: item.breed_id,
+                              quantity: -1
+                            })
+                          )
+                        }
+                        className="cart-icons"
+                      />
+                      <h5>{item.quantity}</h5>
+
+                      <PlusOutlined
+                        onClick={() =>
+                          dispatch(
+                            changeQuantity({ id: item.breed_id, quantity: 1 })
+                          )
+                        }
+                        className="cart-icons"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </Card>
             </div>
-               
-              <div className="cart-qty">
-              <h5>Qty:</h5>
-
-                <MinusOutlined
-                onClick={() =>
-                  dispatch(
-                    changeQuantity({ id: item.breed_id, quantity: -1 })
-                    )
-                  }
-                  className="cart-icons"
-                  />
-                  <h5>{item.quantity}</h5> 
-         
-
-                <PlusOutlined
-                onClick={() =>
-                  dispatch(changeQuantity({ id: item.breed_id, quantity: 1 }))
-                }
-                className="cart-icons"
-                />
-               
-               
-                </div>
-                
-                
-            
-              </div>
-           
-          
-
-             
-            
-             
-
-             
-
-            </Card>
           )
         })
         : null}
 
+      <div className="cart-total-container">
+        <Row>
+          <Col>Subtotal</Col>
+          <Col>${totalCartAmount()}</Col>
+        </Row>
+        <Row>
+          <Col>Shipping</Col>
+          <Col>
+            <strong>FREE</strong>
+          </Col>
+        </Row>
+        <Row>
+          <Col>Est. Taxes & Fees</Col>
+          <Col>${cartTax()}</Col>
+        </Row>
+        <Row>
+          <Col>Est. Total</Col>
+          <Col>
+            <strong>${grandTotal()}</strong>
+          </Col>
+        </Row>
 
-              <div className="cart-total-container">
-
-              <Row>
-                <Col>
-                  Subtotal
-                </Col>
-                <Col>
-             ${totalCartAmount()}.00
-                </Col>
-              </Row>
-              <Row>
-                <Col>Shipping</Col>
-                <Col><strong>FREE</strong></Col>
-              </Row>
-              <Row>
-                <Col>
-                Est. Taxes & Fees</Col>
-                <Col>${cartTax()}</Col>
-              </Row>
-              <Row>
-                <Col>Est. Total</Col>
-                <Col><strong>${grandTotal()}</strong></Col>
-              </Row>
-            
         <div className="total-btns">
           <Row>
-          <Col >
-          <button onClick={toCheckout} className="cart-btn">
-            <IoBagCheckOutline /> Checkout
-          </button>
-        
-          </Col>
-          <Col >
-          <button onClick={keepShopping} className="cart-btn ks">
-            <MdOutlineShopTwo /> Keep Shopping
-          </button>
-          
-          </Col>
-          <Col >
-          <button onClick={onCancel} className="cart-btn">
-            <ImCancelCircle /> Cancel
-          </button>
-          
-          </Col>
+            <Col>
+              <button onClick={toCheckout} className="cart-btn">
+                <IoBagCheckOutline /> Checkout
+              </button>
+            </Col>
+            <Col>
+              <button onClick={keepShopping} className="cart-btn ks">
+                <MdOutlineShopTwo /> Keep Shopping
+              </button>
+            </Col>
+            <Col>
+              <button onClick={onCancel} className="cart-btn">
+                <ImCancelCircle /> Cancel
+              </button>
+            </Col>
           </Row>
         </div>
-              </div>
-     
+      </div>
     </Container>
   )
 }
 
 export default CartItems
+
 
 
 
