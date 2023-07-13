39936a6b807052b0a372b5f562d446e8efd7a0e5
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-07-13 11:30:50 -0500

    styled search container

--- src/components/Animals/Search.js
+++ src/components/Animals/Search.js
@@ -1,16 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { BsSearch } from 'react-icons/bs'
-import SPAGridCard from './SPAGridCard'
-import Card from '../UI/Card'
 import axios from 'axios'
+import './Search.css'
 
-const Search = ({
-  setSearchResult,
-  searchText,
-  setSearchText
-}) => {
-  //   const [searchResult, setSearchResult] = useState([])
-
+const Search = ({ setSearchResult, searchText, setSearchText }) => {
   const searchAnimals = () => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_URL}/animals/getSearch`, {
@@ -21,7 +14,6 @@ const Search = ({
       .then(res => {
         console.log('searchAnimal Result:', ...res.data)
         setSearchResult([...res.data])
-        // setSearchText('')
       })
       .catch(err => {
         console.log('error on searchAnimals:', err)
@@ -32,15 +24,18 @@ const Search = ({
     <div className="search-container">
       <BsSearch className="search-icon" />
       <input
-        placeholder="Search for Animal Breed..."
+        placeholder="Search for animal..."
         type="search"
         className="search-input"
         value={searchText}
         onChange={e => setSearchText(e.target.value)}
       />
-      <button onClick={() => searchAnimals()}>Search</button>
+      <button className="search-btn" onClick={() => searchAnimals()}>
+        Search
+      </button>
     </div>
   )
 }
 
 export default Search
+
