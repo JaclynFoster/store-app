6340d3e5e1e66db090262d49f0bfd757db193c85
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-07-17 15:31:02 -0500

    login form is responsive

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -4,7 +4,10 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import './Login.css'
 import AuthContext from '../../context/userContext'
-import { setLoadingTrue, setLoadingFalse } from '../../redux/slices/isLoadingSlice'
+import {
+  setLoadingTrue,
+  setLoadingFalse
+} from '../../redux/slices/isLoadingSlice'
 import { useDispatch } from 'react-redux'
 const { REACT_APP_BACKEND_URL } = process.env
 
@@ -15,7 +18,7 @@ const LoginForm = () => {
   const [user, setUser] = useState({})
   const props = useContext(AuthContext)
   const navigate = useNavigate()
-  console.log("Context Props:", props)
+  console.log('Context Props:', props)
   const onSignUpHandler = () => {
     navigate('/signup', { replace: true })
   }
@@ -36,12 +39,11 @@ const LoginForm = () => {
             'Username or Password combination is incorrect. Please try again'
           )
         } else {
-          console.log("UserData:", {...res.data.data})
-          props.setUserObject({ ...res.data.data})
+          console.log('UserData:', { ...res.data.data })
+          props.setUserObject({ ...res.data.data })
           dispatch(setLoadingFalse())
           navigate('/', { replace: true })
         }
-
       })
       .catch(err => {
         alert('Credentials invalid. Please try again')
@@ -52,9 +54,8 @@ const LoginForm = () => {
 
   return (
     <div className="login-container">
-         <AuthContext.Provider value={{ id: user.id }}>
-
-            <form key={user.id}  onSubmit={getUserLogin}>
+      <AuthContext.Provider value={{ id: user.id }}>
+        <form className="login-form" key={user.id} onSubmit={getUserLogin}>
           <h1 className="login-title">Login:</h1>
           <Divider />
           <div className="login-space">
@@ -63,7 +64,7 @@ const LoginForm = () => {
               value={username}
               onChange={e => setUsername(e.target.value)}
               className="login-input"
-              />
+            />
 
             <input
               placeholder="Enter Password:..."
@@ -71,25 +72,26 @@ const LoginForm = () => {
               onChange={e => setPassword(e.target.value)}
               className="login-input"
               type="password"
-              />
+            />
             <button className="login-btn" type="submit">
               Submit
             </button>
           </div>
-        <Divider />
-
-      </form>
-              </AuthContext.Provider>
-      
+          <Divider />
+        </form>
+      </AuthContext.Provider>
+      <div className="signup-section">
         <h4>New User?</h4>
-        <button onClick={() => onSignUpHandler()} className="login-btn">
+        <button onClick={() => onSignUpHandler()} className="login-signup-btn">
           Sign Up
         </button>
+      </div>
     </div>
   )
 }
 
 export default LoginForm
+
 
 
 
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -3,18 +3,16 @@ import Layout from '../components/Layout/Layout'
 import LoginForm from '../components/Login/LoginForm'
 import AuthContext from '../context/userContext'
 
-
-const Login = ({user, setUser}) => {
+const Login = ({ user, setUser }) => {
   const props = useContext(AuthContext)
   return (
-   <Layout>
-   
- 
-
-    <LoginForm user={user} setUser={setUser}/>
- 
-   </Layout>
+    <Layout>
+      <div className="login-form-container">
+        <LoginForm user={user} setUser={setUser} />
+      </div>
+    </Layout>
   )
 }
 
 export default Login
+
