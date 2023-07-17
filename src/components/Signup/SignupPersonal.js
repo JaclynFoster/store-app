6136866f3c8ab8fcbd53a2e08e4b5a02dfdad306
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-07-17 16:16:35 -0500

    created shell for personal section in signup

--- src/components/Signup/SignupPersonal.js
+++ src/components/Signup/SignupPersonal.js
@@ -1,2 +1,52 @@
+import React from 'react'
+import { Divider } from 'antd'
 
+const SignupPersonal = ({ signupObj, signupStateHandler }) => {
+  const { firstName, lastName, email, phone } = signupObj
+  return (
+    <div className="profile-section-container">
+      <h5 className="profile-header">Enter Personal Details:</h5>
+      <Divider />
+      <div className="profile-label">
+        <label className="profile-label">First Name:</label>
 
+        <label className="profile-label">Last Name:</label>
+      </div>
+      <div className="profile-input-container">
+        <input
+          value={firstName}
+          onChange={e => signupStateHandler('first_name', e.target.value)}
+          className="profile-input"
+        />
+
+        <input
+          value={lastName}
+          onChange={e => signupStateHandler('last_name', e.target.value)}
+          className="profile-input"
+        />
+      </div>
+      <div className="profile-label">
+        <label className="profile-label">Phone:</label>
+
+        <label className="profile-label">Email:</label>
+      </div>
+      <div className="profile-input-container">
+        <input
+          value={phone}
+          onChange={e => signupStateHandler('phone', e.target.value)}
+          className="profile-input"
+        />
+
+        <input
+          value={email}
+          onChange={e => signupStateHandler('email', e.target.value)}
+          className="profile-input"
+        />
+      </div>
+
+      <Divider />
+    </div>
+  )
+}
+
+export default SignupPersonal
