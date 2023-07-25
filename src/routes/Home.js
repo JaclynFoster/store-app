8c9403573d6c27ac8461c76e6096bc4776c4e8f8
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-07-25 13:15:33 -0500

    updated home section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,90 +12,91 @@ const Home = () => {
     <Layout>
       <div className="home-container">
         <div className="home-image-container">
-
-        <img className="home-image" src={chickenHomePage}/>
-        <EmailCard />
-        <img className="goose" src={gooseHomePage}/>
-      </div>
+          <img className="home-image" src={chickenHomePage} />
+          <EmailCard />
+          <img className="goose" src={gooseHomePage} />
+        </div>
         <div className="homeP">
+          <p>
+            Selling animals online can be a convenient way to connect with
+            potential buyers and find good homes for your furry friends. There
+            are various services available to assist in this process, making it
+            easier for both sellers and buyers. When listing your animals for
+            sale, you can provide detailed descriptions, high-quality photos,
+            and information about their health and temperament. Some platforms
+            even offer video sharing to give potential buyers a better sense of
+            the animal's personality. When it comes to shipping, there are
+            specialized services that ensure safe and comfortable transport for
+            the animals. These services prioritize the well-being of the animals
+            by providing cozy travel accommodations, proper ventilation, and
+            access to food and water during the journey. Additionally, they
+            handle all necessary paperwork and coordinate with the buyer to
+            ensure a smooth and hassle-free delivery. It's important to remember
+            that when selling animals online, responsible and ethical practices
+            should be followed to prioritize the welfare of the animals above
+            all else.
+          </p>
+          <p>
+            To alleviate any worries, I offer a 30-day money back guarantee to
+            ensure buyers' satisfaction. This guarantee not only reinforces my
+            confidence in the animals' health and well-being but also gives
+            buyers peace of mind. I believe in the importance of transparency
+            and honesty, so I promise to deliver animals of great quality,
+            ensuring they are well-cared for and meet the expectations of their
+            new owners.
+          </p>
+          <p>
+            I'd be happy to tell you about my day-to-day routine taking care of
+            the animals I sell online! As an animal lover and passionate pet
+            seller, my mornings usually begin bright and early. After a
+            refreshing cup of coffee to wake me up, I head over to the small and
+            cozy room where I keep my adorable pets. To start the day, I make
+            sure to check on each animal individually, providing them with fresh
+            food and plenty of clean water. I take the time to give each friend
+            some personal attention and affection, whether it's a gentle stroke
+            or a playful interaction. I believe that every animal deserves to
+            feel loved and cared for, even if they are temporarily with me
+            before finding their forever homes. Next, I tackle the important
+            task of cleaning their enclosures or cages. It's all about creating
+            a clean and comfortable environment for the animals to thrive in. I
+            make sure to sanitize their living spaces, change bedding, and
+            provide any necessary essentials like toys or enrichment items.
+            Throughout the day, I carefully monitor their health and well-being.
+            Observing their behavior, appetite, and overall condition helps me
+            ensure that they are happy and healthy. If any concerns arise, I
+            take immediate action to provide proper care, whether it's
+            consulting a veterinarian or adjusting their diet. Additionally, I
+            devote time to photographing each animal in their best light,
+            capturing their unique personalities to showcase their individuality
+            online. I firmly believe that finding the perfect match between pet
+            and owner is essential for a mutually fulfilling relationship, so I
+            do my best to showcase each animal's qualities accurately. As the
+            day comes to an end, I engage in the important task of responding to
+            inquiries and connecting with potential buyers. I want to make sure
+            I find the right homes for my pets, where they will be cherished and
+            given the love they deserve. It's a rewarding and sometimes
+            challenging process, but knowing they will be in safe and loving
+            hands makes it all worthwhile. Overall, my passion for animals and
+            dedication to their well-being drive my daily routine as I strive to
+            provide the best care possible for my little companions. It's a
+            fulfilling journey, witnessing these animals grow and transform into
+            beloved family members.
+          </p>
 
-        <p>
-          Selling animals online can be a convenient way to connect with
-          potential buyers and find good homes for your furry friends. There are
-          various services available to assist in this process, making it easier
-          for both sellers and buyers. When listing your animals for sale, you
-          can provide detailed descriptions, high-quality photos, and
-          information about their health and temperament. Some platforms even
-          offer video sharing to give potential buyers a better sense of the
-          animal's personality. When it comes to shipping, there are specialized
-          services that ensure safe and comfortable transport for the animals.
-          These services prioritize the well-being of the animals by providing
-          cozy travel accommodations, proper ventilation, and access to food and
-          water during the journey. Additionally, they handle all necessary
-          paperwork and coordinate with the buyer to ensure a smooth and
-          hassle-free delivery. It's important to remember that when selling
-          animals online, responsible and ethical practices should be followed
-          to prioritize the welfare of the animals above all else.
-        </p>
-        <p>
-          To alleviate any worries, I offer a 30-day money back guarantee to
-          ensure buyers' satisfaction. This guarantee not only reinforces my
-          confidence in the animals' health and well-being but also gives buyers
-          peace of mind. I believe in the importance of transparency and
-          honesty, so I promise to deliver animals of great quality, ensuring
-          they are well-cared for and meet the expectations of their new owners.
-        </p>
-        <p>
-          I'd be happy to tell you about my day-to-day routine taking care of
-          the animals I sell online! As an animal lover and passionate pet
-          seller, my mornings usually begin bright and early. After a refreshing
-          cup of coffee to wake me up, I head over to the small and cozy room
-          where I keep my adorable pets. To start the day, I make sure to check
-          on each animal individually, providing them with fresh food and plenty
-          of clean water. I take the time to give each friend some personal
-          attention and affection, whether it's a gentle stroke or a playful
-          interaction. I believe that every animal deserves to feel loved and
-          cared for, even if they are temporarily with me before finding their
-          forever homes. Next, I tackle the important task of cleaning their
-          enclosures or cages. It's all about creating a clean and comfortable
-          environment for the animals to thrive in. I make sure to sanitize
-          their living spaces, change bedding, and provide any necessary
-          essentials like toys or enrichment items. Throughout the day, I
-          carefully monitor their health and well-being. Observing their
-          behavior, appetite, and overall condition helps me ensure that they
-          are happy and healthy. If any concerns arise, I take immediate action
-          to provide proper care, whether it's consulting a veterinarian or
-          adjusting their diet. Additionally, I devote time to photographing
-          each animal in their best light, capturing their unique personalities
-          to showcase their individuality online. I firmly believe that finding
-          the perfect match between pet and owner is essential for a mutually
-          fulfilling relationship, so I do my best to showcase each animal's
-          qualities accurately. As the day comes to an end, I engage in the
-          important task of responding to inquiries and connecting with
-          potential buyers. I want to make sure I find the right homes for my
-          pets, where they will be cherished and given the love they deserve.
-          It's a rewarding and sometimes challenging process, but knowing they
-          will be in safe and loving hands makes it all worthwhile. Overall, my
-          passion for animals and dedication to their well-being drive my daily
-          routine as I strive to provide the best care possible for my little
-          companions. It's a fulfilling journey, witnessing these animals grow
-          and transform into beloved family members.
-        </p>
-
-        <p>
-          Waking up at the crack of dawn, tending to the needs of my furry and
-          feathery friends, and watching my business grow has been both
-          fulfilling and challenging. I poured my heart and soul into ensuring
-          the welfare of each and every animal under my care. Through hard work
-          and dedication, I gradually expanded my operation and embraced the
-          digital world. Selling animals online allowed me to reach a wider
-          audience, connecting with like-minded individuals who appreciate the
-          importance of ethical farming practices. Building trust with
-          customers, providing transparency about breeding methods, and offering
-          top-notch customer service were key in establishing my online
-          presence. It's been a remarkable journey, and I am grateful for every
-          step that led me to where I am today.
-        </p>
+          <p>
+            Waking up at the crack of dawn, tending to the needs of my furry and
+            feathery friends, and watching my business grow has been both
+            fulfilling and challenging. I poured my heart and soul into ensuring
+            the welfare of each and every animal under my care. Through hard
+            work and dedication, I gradually expanded my operation and embraced
+            the digital world. Selling animals online allowed me to reach a
+            wider audience, connecting with like-minded individuals who
+            appreciate the importance of ethical farming practices. Building
+            trust with customers, providing transparency about breeding methods,
+            and offering top-notch customer service were key in establishing my
+            online presence. It's been a remarkable journey, and I am grateful
+            for every step that led me to where I am today.
+          </p>
         </div>
       </div>
       <Videos />
@@ -105,6 +106,7 @@ const Home = () => {
 }
 
 export default Home
+
 
 
 
